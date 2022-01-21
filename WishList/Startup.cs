640f8ee4841f287b1e3a8637cf32290115dc1c6a
marty freeman <marty.freeman@healthequity.com>
2022-01-21 15:10:21 -0600

    Module 4

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -8,16 +8,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
+
+
 
 namespace WishList
+
 {
     public class Startup
+
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
 
         }
 
